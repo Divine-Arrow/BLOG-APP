8332c2f4cf2c40a9d8492af78030763a6b279037
Bhupender Singh <bhupenders225@gmail.com>
2018-06-07 00:39:05 +0530

    updating code adding files creating index routes and configured app

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser");
 
+// App configure
 mongoose.connect("mongodb://localhost/blogApp");
-app.set("view engine", ejs);
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
+app.set("view engine", "ejs");
+app.set(bodyParser.urlencoded({
     extended: true
 }));
 
+// Schema
 var blogSchema = new mongoose.Schema({
-    title: String,
+    name: String,
     image: String,
     body: String,
-    created:
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//  mongoose model
+var Blog = mongoose.model("Blog", blogSchema);
+
+//  RESTful routes
+
+// 
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+//  1.> index route
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("Error Found \n", err);
+        } else {
+            res.render("index", {
+                blogs: blogs
+            });
+        }
+    });
 });
 
-time where resume 6:50
-
-
-var Blog = new mongoose("Blog", blogSchema);
+// listen
+app.listen(3000, function () {
+    console.log("Server has Started !");
+});
